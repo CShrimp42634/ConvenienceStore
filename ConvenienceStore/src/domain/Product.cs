abf3c53b558bd40caa5cdabd6213bf98873f387d
CShrimp42634 <CShrimp42634@gmail.com>
2021-06-27 23:45:23 +0900

    add

--- ConvenienceStore/src/domain/Product.cs
+++ ConvenienceStore/src/domain/Product.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace ConvenienceStore.src.domain
 {
+    /// <summary>
+    /// 상품
+    /// </summary>
     class Product
     {
+        /// <summary>
+        /// 상품 이름
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// 상품 가격
+        /// </summary>
+        public int price;
     }
 }
